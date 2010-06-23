6a3feda39d8ae00685298b68f84dc79edc816f50
kitsero <kitsero@yahoo.com>
2010-06-23 04:18:16 +0000

    getting really tired of this system

--- src/actinf.c
+++ src/actinf.c
@@ -2280,7 +2280,7 @@ void new_look(P_char ch, char *argument, int cmd, int room_no)
       }
     
       if((IS_AFFECTED2(ch, AFF2_ULTRAVISION) &&
-          IS_MAGIC_LIGHT(temp) && 
+          IS_MAGIC_LIGHT(temp) &&
           !OLD_RACE_NEUTRAL(ch) &&
           !OLD_RACE_GOOD(ch) &&
           !IS_HARPY(ch) &&
@@ -2293,12 +2293,12 @@ void new_look(P_char ch, char *argument, int cmd, int room_no)
           !IS_HARPY(ch) &&
           !IS_REVENANT(ch) &&
           !IS_DRAGONKIN(ch) &&
-          !IS_HALFORC(ch))
+          !IS_HALFORC(ch)))
       {
-		  send_to_char("&+WThe brightness there hurts your head!\n", ch);
+        send_to_char("&+WThe brightness there hurts your head!\n", ch);
         return;
       }
-      else if((IS_MAGIC_DARK(temp)) &&
+      else if((IS_MAGIC_DARK(temp) &&
                !OLD_RACE_NEUTRAL(ch)) &&
                OLD_RACE_GOOD(ch) &&
                !IS_HARPY(ch) &&
